8d6c8dd3183cad4ffbe57f20b3d73d1187399cd0
NikolayZ <zharikov.nikolay2@gmail.com>
2019-04-20 18:48:29 +0300

    new tests added

--- tests/Commands/TestSum2Num/Sum2NumCommandTest.php
+++ tests/Commands/TestSum2Num/Sum2NumCommandTest.php
@@ -19,9 +19,9 @@ class Sum2NumCommandTest extends TestCase
         $registry = $this->createMock('\CharlotteDunois\Livia\CommandRegistry');
         $types = $this->createMock('\CharlotteDunois\Yasmin\Utils\Collection');
 
-        $types->expects($this->exactly(1))->method('has')->willReturn(true);
-        $registry->expects($this->exactly(2))->method('__get')->with('types')->willReturn($types);
-        $this->client->expects($this->exactly(2))->method('__get')->with('registry')->willReturn($registry);
+        $types->expects($this->exactly(2))->method('has')->willReturn(true);
+        $registry->expects($this->exactly(4))->method('__get')->with('types')->willReturn($types);
+        $this->client->expects($this->exactly(4))->method('__get')->with('registry')->willReturn($registry);
 
         $this->command = $commandCreate($this->client);
 
@@ -31,13 +31,13 @@ class Sum2NumCommandTest extends TestCase
     public function testSum2NumBasics()
     {
        $this->assertEquals($this->command->name, 'sum2num');
-       $this->assertEquals($this->command->description, '...');
+       $this->assertEquals($this->command->description, 'The command takes 2 numbers and returns the sum of them.');
        $this->assertEquals($this->command->groupID, 'utils');
     }
 
     public function testSum2NumArguments()
     {
-       $this->assertEquals(sizeof($this->command->args), 1);
+       $this->assertEquals(sizeof($this->command->args), 2);
        $this->assertArrayHasKey('key', $this->command->args[0]);
        $this->assertArrayHasKey('label', $this->command->args[0]);
        $this->assertArrayHasKey('prompt', $this->command->args[0]);
@@ -52,11 +52,34 @@ class Sum2NumCommandTest extends TestCase
 
         $commandMessage->expects($this->once())->method('say')->with('...')->willReturn($promise);
  
-        $this->command->run($commandMessage, new ArrayObject(), false);
+        $this->command->run($commandMessage, new ArrayObject(['first_arg' => '1','second_arg' => '2']), false);
+    }
+
+
+    /**
+     * @dataProvider myProvider
+     */
+    public function testSimpleConditions($a, $b, $expected)
+    {
+        $this->assertSame($expected, $a+$b);
+    }
+
+    public function myProvider()
+    {
+        return [
+            [0,0,0],
+            [0,1,1],
+            [1,0,1],
+            [1,1,2],
+            [2,2,4],
+            [2,3,5]
+        ];
     }
 
     public function __sleep()
     {
         $this->command = null;
     }
+
 }
+
